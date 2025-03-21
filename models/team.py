257d7b2a241bea1e9f88fa4a997adbbd68aa9e80
RichinW0C <richardwalace2020@gmail.com>
2025-03-21 15:53:21 -0300

    Mudança model team

--- models/team.py
+++ models/team.py
@@ -4,12 +4,12 @@ from sqlalchemy.sql import func
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_employee_one = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
-    id_employee_two = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    id_employee_two = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=True)
     date_register = db.Column(db.Date, default=func.current_date(), nullable=False)
     employee_one = db.relationship('Employee', foreign_keys=[id_employee_one])
     employee_two = db.relationship('Employee', foreign_keys=[id_employee_two])
 
-    def __init__(self, id_employee_one, id_employee_two):
+    def __init__(self, id_employee_one, id_employee_two=None):
         self.id_employee_one = id_employee_one
         self.id_employee_two = id_employee_two
 
